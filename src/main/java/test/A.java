c70c4310c649ac9d18327c5d4e434b9f5d62d546
seungwonKim98 <godswan1998@gmail.com>
2021-01-26 18:02:23 +0900

    .

---
+++ src/main/java/test/A.java
@@ -0,0 +1,13 @@
+package test;
+
+public class A {
+  B obj = new B();
+
+  void m1() {
+    obj.y();
+  }
+
+  void m2() {
+    obj.y();
+  }
+}
---
+++ src/main/java/test/Test.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Test {
+  public static void main(String[] args) {
+    강사 a = new 강사();
+
+    프로젝트 b = new 프로젝트();
+    a.강의하다(b);
+    a.발표하다(b);
+
+
+  }
+}
